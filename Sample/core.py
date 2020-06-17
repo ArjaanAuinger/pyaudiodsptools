d756ee2137c43bd121e079d24a04a9219c1b2a4a
Arjaan Auinger <arjaan.auinger@gmail.com>
2020-06-17 19:31:13 +0200

    0.2
    Added functions

--- Sample/core.py
+++ Sample/core.py
@@ -8,35 +8,31 @@ import math
 import matplotlib.pyplot as pyplot
 import scipy
 import copy
+import wave
+import struct
 
 #512 samples@44.1 kHz = 11.7ms = 0.00117s
 print(numpy.finfo('float64').max)# 1.79
 print(numpy.finfo('float64').min)# -1.79
+print(numpy.iinfo('int16').max)# 32767
+print(numpy.iinfo('int16').min)# -32767
 
-def createsinewave(sin_sample_rate, sin_frequency, sin_buffer_size):
+def CreateSinewave(sin_sample_rate, sin_frequency, sin_buffer_size):
     sin_time_array = numpy.arange(sin_buffer_size)
-    sin_amplitude_array = numpy.sin(2 * numpy.pi * sin_frequency*sin_time_array/sin_sample_rate)
+    sin_amplitude_array = numpy.int16(32767*numpy.sin(2 * numpy.pi * sin_frequency*sin_time_array/sin_sample_rate))
     return (sin_time_array, sin_amplitude_array)
-
-
-def createsquarewave(square_sample_rate, square_frequency, square_buffer_size):
+def CreateSquarewave(square_sample_rate, square_frequency, square_buffer_size):
     square_time_array = numpy.arange(square_buffer_size)
-    square_amplitude_array = numpy.sin(2 * numpy.pi * square_frequency * square_time_array / square_sample_rate)
+    square_amplitude_array = numpy.int16(32767*numpy.sin(2 * numpy.pi * square_frequency * square_time_array / square_sample_rate))
     #print (square_amplitude_array)
     for sample in square_time_array:
             if square_amplitude_array[sample] > 0:
-                square_amplitude_array[sample] = 1.0
+                square_amplitude_array[sample] = 32767
             if square_amplitude_array[sample] < 0:
-                square_amplitude_array[sample] = -1.0
+                square_amplitude_array[sample] = -32767
     #print (square_amplitude_array)
     return (square_time_array, square_amplitude_array)
-"""
-def createwhitenoise(whitenoise_buffer_size):
-    samples = numpy.random.normal(0, 1, size=whitenoise_buffer_size)
-    print (samples)
-"""
-
-def createwhitenoise(buffer_size, sample_rate):
+def CreateWhitenoise(buffer_size, sample_rate):
     whitenoise_time_array = numpy.arange(buffer_size)
     freqs = numpy.abs(numpy.fft.fftfreq(buffer_size, 1/sample_rate))
     f = numpy.zeros(buffer_size)
@@ -52,20 +48,21 @@ def createwhitenoise(buffer_size, sample_rate):
         f[-1:-1 - Np:-1] = numpy.conj(f[1:Np + 1])
         return (numpy.fft.ifft(f).real*5)
 
-    whitenoise_amplitude_array = fftnoise(f)
+    whitenoise_amplitude_array = numpy.int16(32767*fftnoise(f))
 
     return whitenoise_time_array,whitenoise_amplitude_array
 
 
-def converfloatto16bit(float_array_input):
-    float_array_input = numpy.where(float_array_input < 1, float_array_input, 1)
-    print(float_array_input)
-    float_array_input = numpy.where(float_array_input > -1, float_array_input, -1)
-
-    float_array_output = numpy.int16(float_array_input * (2 ** 15 - 1))
+def ConvertdBuTo16Bit(float_array_input):
+    float_array_input = numpy.where(float_array_input < 1.736, float_array_input, 1.736)
+    float_array_input = numpy.where(float_array_input > -1.736, float_array_input, -1.736)
+    float_array_output = numpy.int16(float_array_input * ((2 ** 15 - 1)/1.736))
+    return float_array_output
+def Convert16BitTodBu(float_array_input):
+    float_array_output = numpy.float64((float_array_input/32767)*1.736)
     return float_array_output
 
-def dBVinfo(float_array_input):
+def InfodBV(float_array_input):
     count = 0
     added_sample_value = 0.0
     average_sample_value = 0.0
@@ -75,8 +72,7 @@ def dBVinfo(float_array_input):
     amplitude = average_sample_value/1
     dBV = 20 * math.log10(amplitude)
     return dBV
-
-def dB16bitinfo(float_array_input):
+def InfodBV16Bit(float_array_input):
     count = 0
     added_sample_value = 0.0
     average_sample_value = 0.0
@@ -87,17 +83,103 @@ def dB16bitinfo(float_array_input):
     dB16 = 20 * math.log10(amplitude)
     return dB16
 
+def MonoWavToNumpy16BitInt(wav_file_path):
+    wav_file = wave.open(wav_file_path)
+    samples = wav_file.getnframes()
+    audio = wav_file.readframes(samples)
+    audio_as_numpy_array = np.frombuffer(audio, dtype=np.int16)
+    return(audio_as_numpy_array)
+def Numpy16BitIntToMonoWav(filename, data):
+    """
+    Write a numpy array as a WAV file
 
-x,y = createwhitenoise(512,44100)
+    Parameters
+    ----------
+    filename : string or open file handle
+        Output wav file
+    rate : int
+        The sample rate (in samples/sec).
+    data : ndarray
+        A 1-D or 2-D numpy array of either integer or float data-type.
 
-x2,y2 = createsinewave(44100,1000,512)
+    Notes
+    -----
+    * The file can be an open file or a filename.
 
-x3,y3 = createsquarewave(44100,1000,512)
+    * Writes a simple uncompressed WAV file.
+    * The bits-per-sample will be determined by the data-type.
+    * To write multiple-channels, use a 2-D array of shape
+      (Nsamples, Nchannels).
+
+    """
+    if hasattr(filename,'write'):
+        fid = filename
+    else:
+        fid = open(filename, 'wb')
+
+    try:
+        dkind = data.dtype.kind
+        if not (dkind == 'i' or dkind == 'f' or (dkind == 'u' and data.dtype.itemsize == 1)):
+            raise ValueError("Unsupported data type '%s'" % data.dtype)
+
+        fid.write(b'RIFF')
+        fid.write(b'\x00\x00\x00\x00')
+        fid.write(b'WAVE')
+        # fmt chunk
+        fid.write(b'fmt ')
+        if dkind == 'f':
+            comp = 3
+        else:
+            comp = 1
+        if data.ndim == 1:
+            noc = 1
+        else:
+            noc = data.shape[1]
+        bits = data.dtype.itemsize * 8
+        sbytes = 44100*(bits // 8)*noc
+        ba = noc * (bits // 8)
+        fid.write(struct.pack('<ihHIIHH', 16, comp, noc, 44100, sbytes, ba, bits))
+        # data chunk
+        fid.write(b'data')
+        fid.write(struct.pack('<i', data.nbytes))
+        if data.dtype.byteorder == '>' or (data.dtype.byteorder == '=' and sys.byteorder == 'big'):
+            data = data.byteswap()
+        _array_tofile(fid, data)
+
+        # Determine file size and place it in correct
+        #  position at start of the file.
+        size = fid.tell()
+        fid.seek(4)
+        fid.write(struct.pack('<i', size-8))
+
+    finally:
+        if not hasattr(filename,'write'):
+            fid.close()
+        else:
+            fid.seek(0)
+    return
+
+if sys.version_info[0] >= 3:
+    def _array_tofile(fid, data):
+        # ravel gives a c-contiguous buffer
+        fid.write(data.ravel().view('b').data)
+else:
+    def _array_tofile(fid, data):
+        fid.write(data.tostring())
+
+
+
+
+x,y = CreateWhitenoise(512,44100)
+
+x2,y2 = CreateSinewave(44100,1000,88200)
+
+x3,y3 = CreateSquarewave(44100,1000,512)
 
 
 
 start = timeit.default_timer()
-y2=converfloatto16bit(y2)
+Numpy16BitIntToMonoWav('test.wav', y2)
 stop = timeit.default_timer()
 print('Time: ', (stop - start)*1000, 'ms')
 
@@ -106,7 +188,6 @@ print('Time: ', (stop - start)*1000, 'ms')
 pyplot.plot(x, y)
 pyplot.plot(x2,y2)
 pyplot.plot(x3,y3)
-#pyplot.plot(x2,y2)
 
 pyplot.show()
 
