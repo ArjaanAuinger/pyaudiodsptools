494ce9c6c0e73688c29d971be9fd405089455425
Arjaan Auinger <arjaan.auinger@gmail.com>
2021-01-04 19:36:34 +0100

    0.7.7q
    Just lol

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sphinx_bootstrap_theme
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../pyAudioDspTools'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
